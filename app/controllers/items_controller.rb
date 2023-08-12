875b818c0a03bf771114438cde59a45c66b917b6
diego <guatemaladiegojose@gmail.com>
2023-08-11 20:53:39 -0400

    completed GET /users/:user_id/items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,28 @@
 class ItemsController < ApplicationController
 
+ rescue_from ActiveRecord::RecordNotFound,  with: :set_alarm
+  # def index
+  #   items = Item.all
+  #   render json: items, include: :user
+  # end
+
   def index
-    items = Item.all
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+      else
+      items = Item.all
+    end
     render json: items, include: :user
   end
+
+
+  private
+
+  def set_alarm
+      render json: {error: 404}, status: :not_found
+  end
+
+
 
 end
